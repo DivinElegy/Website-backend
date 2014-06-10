b6fc85ee523f430c5afa6ff9c15073b3701de442
Cameron Ball <c.ball1729@gmail.com>
2014-06-10 23:25:23 +0800

    Progress on #1. Still need to implement domain objects for Artist, bpm etc but at least I now have a starting point.

---
+++ public_html/vendor/DivinElegy/Simfile.php
@@ -0,0 +1,46 @@
+<?php
+namespace DivinElegy;
+
+class Simfile
+{
+    //str - simfile title
+    protected $title;
+    
+    //obj - reerence to song artist
+    protected $artist;
+    
+    //obj - reference to uploader
+    protected $uploader;
+    
+    //obj - reference to bpm object
+    protected $bpm;
+    
+    //bool - does the chart have stops
+    protected $stops;
+    
+    //bool - does the chart have fgChanges
+    protected $fgChanges;
+    
+    //bool - does the charg have bgChanges
+    protected $bgChanges;
+    
+    public function __construct($title)
+    {
+        $this->setTitle($title);
+    }
+    
+    protected function setTitle($title) 
+    {
+        if (empty($title))
+        {
+            throw new \InvalidArgumentException('Simfile title cannot be empty');
+        }
+        
+        $this->title = $title;
+    }
+    
+    public function setArtist(Artist $artist)
+    {
+        $this->artist = $artist;
+    }
+}
