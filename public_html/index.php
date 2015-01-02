e0dca0223154254eeeddbca1f726c9cc945c6dbc
Cameron Ball <cameron@getapproved.com.au>
2015-01-02 10:31:34 +0800

    Cleanup index

--- public_html/index.php
+++ public_html/index.php
@@ -2,40 +2,44 @@
 
 require_once('../vendor/autoload.php');
 
-$config = require('../config/app.php');
+//Timezone biz
+date_default_timezone_set('UTC');
 
-// Allow these origins to do cross domain JS.
-header("Access-Control-Allow-Origin: " . $config['allow-origin']);
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+//DI biz
+$containerBuilder = new DI\ContainerBuilder();
+$containerBuilder->addDefinitions('../config/DI.php');
+$containerBuilder->useAutowiring(true);
+$container = $containerBuilder->build();
 
-// Nice exceptions
-if($config['mode'] == 'production')
+$config = $container->get('Services\IConfigManager');
+$router = $container->get('Services\Routing\IRouter');
+
+//Exception biz
+if($config->getDirective('mode') == 'production')
 {
     ini_set('display_errors', 0);
     set_exception_handler(array('\Services\StatusReporter', 'exception'));
 } else {
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
+}    
+
+//CORS biz
+$request_headers = apache_request_headers();
+$http_origin = isset($request_headers['Origin']) ? $request_headers['Origin'] : null;
+
+if(in_array($http_origin, $config->getDirective('allowed-origins')))
+{
+    header("Access-Control-Allow-Origin: " . $http_origin);
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 }
 
-// Everything time related should be UTC+0 based
-date_default_timezone_set('UTC');
-
-// Set up the DI container
-$containerBuilder = new DI\ContainerBuilder();
-$containerBuilder->addDefinitions('../config/DI.php');
-$containerBuilder->useAutowiring(true);
-
-$container = $containerBuilder->build();
-
-/* @var $router Services\Routing\IRouter */
-$router = $container->get('Services\Routing\IRouter');
-
+//Controller biz
 $controllerName= $router->getControllerName();
 $controllerAction = $router->getActionName();
 $controllerActionArgs = $router->getActionArgs();
-
 $controller = $container->get('Controllers\\' . ucfirst($controllerName) . 'Controller' );
 
-// Last thing to do, call the action on the specified controller.
+//Last thing to do, call the action on the specified controller.
+//Biz biz
 call_user_func_array(array($controller, $controllerAction . 'Action'), $controllerActionArgs);
