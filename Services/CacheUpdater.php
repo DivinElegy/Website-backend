d20cb75212e273a64c158ef35f6f61674a691621
Cameron Ball <c.ball1729@gmail.com>
2015-02-21 16:51:19 +0800

    This time for real...

--- Services/CacheUpdater.php
+++ Services/CacheUpdater.php
@@ -4,18 +4,25 @@ namespace Services;
 
 use Services\ICacheUpdater;
 use Domain\Entities\StepMania\IPack;
+use DataAccess\StepMania\IPackRepository;
 use Domain\Util;
 
 class CacheUpdater implements ICacheUpdater
 {
     private $_json;
+    private $_packRepository;
     
-    public function __construct() {
+    public function __construct(IPackRepository $packRepository) {
         $this->_json = json_decode(file_get_contents('../SimfileCache/simfiles.json'), true);
+        $this->_packRepository = $packRepository;
     }
     
     public function insert(IPack $pack)
     {
+        //XXX: Tricky, when the pack comes in it may not be fully populated
+        //we are interested in keeping the cache in sync with the DB, so
+        //reload it from the DB to ensure we have everything.
+        $pack = $this->_packRepository->findById($pack->getId());
         $this->_json['packs'][] = Util::packToArray($pack);
         return $this;
     }
